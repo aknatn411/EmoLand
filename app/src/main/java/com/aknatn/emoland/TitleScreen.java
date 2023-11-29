81a2ad882eb27271c3b1c3404f915251ed0e2679
Chris Hernandez <chris.j.hernandez@gmail.com>
2023-11-29 13:48:28 -0800

    Initial commit

---
+++ app/src/main/java/com/aknatn/emoland/TitleScreen.java
@@ -0,0 +1,20 @@
+package com.aknatn.emoland;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.TextViewCompat;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TitleScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+
+        TextView title = (TextView)findViewById(R.id.titleTextView);
+        title.setText("Emo Land");
+    }
+
+}
