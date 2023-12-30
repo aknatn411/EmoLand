b96b100c08ca0a0498353f971cfc04619f50ec31
aknatn411 <aknatn411@gmail.com>
2023-12-30 13:14:56 -0600

    Update poetry builder.

--- app/src/main/java/com/aknatn/emoland/PoetryBuilder.java
+++ app/src/main/java/com/aknatn/emoland/PoetryBuilder.java
@@ -7,4 +7,11 @@ public class PoetryBuilder {
     List<String> Line2Options;
     List<String> Line3Options;
     List<String> Line4Options;
+
+    public PoetryBuilder(){
+        Line1Options.add("This is the start");
+        Line2Options.add("This is line 2");
+        Line3Options.add("This is line 3");
+        Line4Options.add("This is the end");
+    }
 }
