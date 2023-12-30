8e40c18e01b63e9dab2dcade77ec42642a851b19
aknatn411 <aknatn411@gmail.com>
2023-12-30 13:03:40 -0600

    Start poetry builder.

---
+++ app/src/main/java/com/aknatn/emoland/PoetryBuilder.java
@@ -0,0 +1,10 @@
+package com.aknatn.emoland;
+
+import java.util.List;
+
+public class PoetryBuilder {
+    List<String> Line1Options;
+    List<String> Line2Options;
+    List<String> Line3Options;
+    List<String> Line4Options;
+}
